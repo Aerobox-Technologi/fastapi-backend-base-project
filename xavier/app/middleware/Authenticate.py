c82890c0959d02cec89763c8a46daa1b6957d6a7
niko <nikofigit@gmail.com>
2022-09-10 23:35:24 +0700

    delete middleware

--- xavier/app/middleware/Authenticate.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter,FastAPI, Depends, HTTPException,Request
-
-
-class Auth:
-    async def getBody(request: Request, call_next):
-        body = await request.body()
-        print(body)
-        response = await call_next(request)
-        return response
